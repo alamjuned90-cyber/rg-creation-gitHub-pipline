a99321d7bec67d9f285bcb20628f4bfbd74a330c
alamjuned90-cyber <alam.juned90@gmail.com>
2026-06-24 23:32:40 +0530

    reverted via pipline

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "bc901ec6-6856-482d-8884-ffc26339ee5b"
+}
